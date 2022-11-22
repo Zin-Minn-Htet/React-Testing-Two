8acbf13cf431e7e3af1807a264b8a04d8982f2ae
Zin Min Htet <zinminn34119@gmail.com>
2022-11-22 16:09:18 +0630

    Counter Branch

--- src/Component/ACompo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ACompo() {
-  return (
-    <div>ACompo</div>
-  )
-}
-
-export default ACompo
--- src/Component/BCompo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function BCompo() {
-  return (
-    <div>BCompo</div>
-  )
-}
-
-export default BCompo
